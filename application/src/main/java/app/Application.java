232fdda78ddc078aee452001ce7a9458f453b608
Mikita Karaliou <mikita.karaliou@pandadoc.com>
2021-03-04 13:54:16 +0300

    init

---
+++ application/src/main/java/app/Application.java
@@ -0,0 +1,23 @@
+package app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication(scanBasePackages = "app")
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(applicationContext.getBean("test-bean"));
+    }
+}
---
+++ module/src/main/java/app/TestBean.java
@@ -0,0 +1,7 @@
+package app;
+
+import org.springframework.stereotype.Component;
+
+@Component("test-bean")
+public class TestBean {
+}
